b9b5451f9b75a3bae5401670ec80a67fadcd2a8b
elias-92 <martinezelias17@gmail.com>
2023-06-30 04:53:23 -0300

    create functions using context and use databases

--- src/App.js
+++ src/App.js
@@ -1,33 +1,88 @@
-import './app.css';
-import styled from '@emotion/styled';
-import Lists from './components/Lists';
-import backgroundImg from './img/imgFondo.jpg';
-import AddCardOrList from './components/AddCardOrList';
+import './app.css'
+import styled from '@emotion/styled'
+import Lists from './components/Lists'
+import backgroundImg from './img/imgFondo.jpg'
+import AddCardOrList from './components/AddCardOrList'
+import mockData from './mockData.js'
+import { useState } from 'react'
+import ContextApi from './ContextApi'
 
 function App() {
-	return (
-		<Root>
-			<ListsContainer>
-				<Lists />
-				<Lists />
-				<Lists />
-				<div>
-					<AddCardOrList type="list" />
-				</div>
-			</ListsContainer>
-		</Root>
-	);
+  const [data, setData] = useState(mockData)
+
+  const updatedTitle = (newTitle, listId) => {
+    const list = data.lists[listId]
+    list.title = newTitle
+    setData({
+      ...data,
+      lists: {
+        ...data.lists,
+        [listId]: list
+      }
+    })
+  }
+
+  const addCard = (title, listId) => {
+    const newCard = {
+      id: +new Date(),
+      title
+    }
+    const list = data.lists[listId]
+    list.cards = [...list.cards, newCard]
+    setData({
+      ...data,
+      lists: {
+        ...data.lists,
+        [listId]: list
+      }
+    })
+  }
+  const addList = (title) => {
+    const newListId = +new Date()
+    setData({
+      listIds: [...data.listIds, newListId],
+      lists: {
+        ...data.lists,
+        [newListId]: {
+          id: newListId,
+          title,
+          cards: []
+        }
+      }
+    })
+  }
+
+  return (
+    <ContextApi.Provider value={{ updatedTitle, addCard, addList }}>
+      <Root>
+        <ListsContainer>
+          {data.listIds.map((listId) => {
+            const list = data.lists[listId]
+            return (
+              <Lists
+                list={list}
+                key={listId}
+              />
+            )
+          })}
+          <div>
+            <AddCardOrList type="list" />
+          </div>
+        </ListsContainer>
+      </Root>
+    </ContextApi.Provider>
+  )
 }
 const Root = styled.div`
-	height: 100vh;
-	overflow-y: auto;
-	margin: 0 1rem 0 0;
-	background-image: url(${backgroundImg});
-	background-position: center;
-	background-repeat: no-repeat;
-	background-size: cover;
-`;
+  height: 100vh;
+  overflow-y: auto;
+  margin: 0 1rem 0 0;
+  background-image: url(${backgroundImg});
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+`
 const ListsContainer = styled.div`
-	display: flex;
-`;
-export default App;
+  display: flex;
+`
+export default App
